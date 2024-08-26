e2696f64d04dce51027d4c2fe58bdb63ebcc5c0d
Mathias Aschwanden <mathias.aschwanden@gmail.com>
2024-08-26 02:14:07 +0200

    Activate hardtime plugin

--- config/plugins.nix
+++ config/plugins.nix
@@ -281,30 +281,28 @@
       ];
     };
 
-    # hardtime = {
-    #   enable = true;
-    #   enabled = true;
-    #   disableMouse = true;
-    #   disabledFiletypes = [ "Oil" ];
-    #   hint = true;
-    #   maxCount = 4;
-    #   maxTime = 1000;
-    #   restrictionMode = "hint";
-    #   restrictedKeys = {
-    #     "h" = [ "n" "x" ];
-    #     "j" = [ "n" "x" ];
-    #     "k" = [ "n" "x" ];
-    #     "l" = [ "n" "x" ];
-    #     "-" = [ "n" "x" ];
-    #     "+" = [ "n" "x" ];
-    #     "gj" = [ "n" "x" ];
-    #     "gk" = [ "n" "x" ];
-    #     "<CR>" = [ "n" "x" ];
-    #     "<C-M>" = [ "n" "x" ];
-    #     "<C-N>" = [ "n" "x" ];
-    #     "<C-P>" = [ "n" "x" ];
-    #   };
-    # };
+    hardtime = {
+      enable = true;
+      enabled = true;
+      disableMouse = true;
+      disabledFiletypes = [ "Oil" ];
+      hint = true;
+      maxCount = 4;
+      maxTime = 1000;
+      restrictionMode = "hint";
+      restrictedKeys = {
+        "m" = [ "n" "v" "x" ];
+        "n" = [ "n" "v" "x" ];
+        "e" = [ "n" "v" "x" ];
+        "i" = [ "n" "v" "x" ];
+        "-" = [ "n" "x" ];
+        "+" = [ "n" "x" ];
+        "<CR>" = [ "n" "x" ];
+        "<C-M>" = [ "n" "x" ];
+        "<C-N>" = [ "n" "x" ];
+        "<C-P>" = [ "n" "x" ];
+      };
+    };
 
     # Nix expressions in Neovim
     nix = {
