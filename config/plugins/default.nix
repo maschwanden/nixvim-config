dabd63be92f4c268a4ff51a3ad78d98b3d5eed40
Mathias Aschwanden <mathias.aschwanden@gmail.com>
2024-10-04 14:02:37 +0200

    Remove markview.nvim plugin
    The plugin becomes quite annoying when editing Markdown files, thus
    remove it for now.

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -423,20 +423,20 @@
 
   extraPlugins = with pkgs.vimPlugins;
     [
-      headlines-nvim # Should load this in at the opening of filetypes that require this, namely Markdown.
+      # headlines-nvim # Should load this in at the opening of filetypes that require this, namely Markdown.
       elixir-tools-nvim
     ]
     ++ [
-      (pkgs.vimUtils.buildVimPlugin {
-        pname = "markview.nvim";
-        version = "0.0.1";
-        src = pkgs.fetchFromGitHub {
-          owner = "OXY2DEV";
-          repo = "markview.nvim";
-          rev = "a959d77ca7e9f05175e3ee4e582db40b338c9164";
-          hash = "sha256-w6yn8aNcJMLRbzaRuj3gj4x2J/20wUROLM6j39wpZek=";
-        };
-      })
+      #   (pkgs.vimUtils.buildVimPlugin {
+      #     pname = "markview.nvim";
+      #     version = "0.0.1";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "OXY2DEV";
+      #       repo = "markview.nvim";
+      #       rev = "a959d77ca7e9f05175e3ee4e582db40b338c9164";
+      #       hash = "sha256-w6yn8aNcJMLRbzaRuj3gj4x2J/20wUROLM6j39wpZek=";
+      #     };
+      #   })
       # (pkgs.vimUtils.buildVimPlugin {
       #   pname = "accelerated-jk";
       #   src = pkgs.fetchFromGitHub {
