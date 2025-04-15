adcd5c804f6c446d44bc2da8b7a6eda4da76513f
Mathias Aschwanden <mathias.aschwanden@gmail.com>
2025-04-15 23:40:57 +0200

    Disable useless providers

--- config/options.nix
+++ config/options.nix
@@ -49,7 +49,14 @@
     W.bang = true;
   };
 
-  globals.mapleader = " ";
+  globals = {
+    mapleader = " ";
+
+    # Disable useless providers
+    loaded_ruby_provider = 0; # Ruby
+    loaded_perl_provider = 0; # Perl
+    loaded_python_provider = 0; # Python 2
+  };
 
   # autoCmd = [
   #   {
