deeafd39298ad2095a1657dfe7c546ab2c5f0615
Mathias Aschwanden <mathias.aschwanden@gmail.com>
2025-04-15 23:41:50 +0200

    Formatting

--- config/plugins/copilot-cmp.nix
+++ config/plugins/copilot-cmp.nix
@@ -2,13 +2,16 @@
   plugins.copilot-cmp = {
     enable = true;
   };
+
   plugins.copilot-lua = {
     enable = true;
-    suggestion = {
-      enabled = false;
-    };
-    panel = {
-      enabled = false;
+    settings = {
+      suggestion = {
+        enabled = false;
+      };
+      panel = {
+        enabled = false;
+      };
     };
   };
 
