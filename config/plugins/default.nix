f8eba2992459cb5a27033cc0c8f161926749424c
Mathias Aschwanden <mathias.aschwanden@meteotest.ch>
2025-05-01 15:41:09 +0200

    Add interestingwords plugin

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -274,6 +274,16 @@
        }),
      })
 
+     require("interestingwords").setup {
+        colors = { '#aeee00', '#ff0000', '#0000ff', '#b88823', '#ffa724', '#ff2c4b' },
+        search_count = true,
+        navigation = false,
+        scroll_center = true,
+        color_key = "<leader>k",
+        cancel_color_key = "<leader>K",
+        select_mode = "random",  -- random or loop
+    }
+
   '';
 
   colorschemes.catppuccin = {
@@ -313,21 +323,22 @@
     };
   };
 
-  extraPlugins = with pkgs.vimPlugins; [
-    # headlines-nvim # Should load this in at the opening of filetypes that require this, namely Markdown.
-  ];
-  # ++ [
-  #   (pkgs.vimUtils.buildVimPlugin {
-  #     pname = "markview.nvim";
-  #     version = "0.0.1";
-  #     src = pkgs.fetchFromGitHub {
-  #       owner = "OXY2DEV";
-  #       repo = "markview.nvim";
-  #       rev = "a959d77ca7e9f05175e3ee4e582db40b338c9164";
-  #       hash = "sha256-w6yn8aNcJMLRbzaRuj3gj4x2J/20wUROLM6j39wpZek=";
-  #     };
-  #   })
-  # ];
+  extraPlugins = with pkgs.vimPlugins;
+    [
+      # headlines-nvim # Should load this in at the opening of filetypes that require this, namely Markdown.
+    ]
+    ++ [
+      (pkgs.vimUtils.buildVimPlugin {
+        pname = "nvim-interestingwords";
+        version = "0.0.1";
+        src = pkgs.fetchFromGitHub {
+          owner = "Mr-LLLLL";
+          repo = "interestingwords.nvim";
+          rev = "fa7796353266ce9a5de6a95a8e332814f09a5514";
+          hash = "sha256-Au05deUPKprlq5cnmuXtREcQF96B3t2r6f6kMS6Uo6w=";
+        };
+      })
+    ];
 
   keymaps = [
     # Commentary bindings
