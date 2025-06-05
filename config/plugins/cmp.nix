c905c24aecafd8aec222deb4a39b28653e99b85e
Mathias Aschwanden <mathias.aschwanden@gmail.com>
2025-06-05 12:04:06 +0200

    Add keymap to select previous item in cmp-selection
    Also: Autoformatting was applied.

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -28,17 +28,17 @@
       enable = true;
       settings = {
         autoEnableSources = true;
-        experimental = { ghost_text = true; };
+        experimental = {ghost_text = true;};
         performance = {
           debounce = 60;
           fetchingTimeout = 200;
           maxViewEntries = 30;
         };
-        snippet = { expand = "luasnip"; };
-        formatting = { fields = [ "kind" "abbr" "menu" ]; };
+        snippet = {expand = "luasnip";};
+        formatting = {fields = ["kind" "abbr" "menu"];};
         sources = [
-          { name = "nvim_lsp"; }
-          { name = "emoji"; }
+          {name = "nvim_lsp";}
+          {name = "emoji";}
           {
             name = "buffer"; # text within current buffer
             option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
@@ -56,17 +56,15 @@
         ];
 
         window = {
-          completion = { border = "solid"; };
-          documentation = { border = "solid"; };
+          completion = {border = "solid";};
+          documentation = {border = "solid";};
         };
 
         mapping = {
           "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          "<C-e>" = "cmp.mapping.select_prev_item()";
           "<C-Space>" = "cmp.mapping.complete()";
           "<CR>" = "cmp.mapping.confirm({ select = true })";
           "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
