2adb8c6f9dd0155f8bc59685df19da36720ae33e
Mathias Aschwanden <mathias.aschwanden@meteotest.ch>
2026-01-05 14:14:45 +0100

    Update flake inputs

--- config/plugins/neo-tree.nix
+++ config/plugins/neo-tree.nix
@@ -1,34 +1,38 @@
 {
   plugins.neo-tree = {
     enable = true;
-    enableDiagnostics = true;
-    enableGitStatus = true;
-    enableModifiedMarkers = true;
-    enableRefreshOnWrite = true;
-    closeIfLastWindow = true;
-    popupBorderStyle = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
-    buffers = {
-      bindToCwd = true;
-      followCurrentFile = {
-        enabled = true;
+
+    settings = {
+      buffers = {
+        bindToCwd = true;
+        followCurrentFile = {
+          enabled = true;
+        };
       };
-    };
-    filesystem = {
-      followCurrentFile = {
-        enabled = true;
+      enableDiagnostics = true;
+      enableGitStatus = true;
+      enableModifiedMarkers = true;
+      enableRefreshOnWrite = true;
+      filesystem = {
+        followCurrentFile = {
+          enabled = true;
+        };
       };
-    };
-    window = {
-      width = 40;
-      height = 15;
-      autoExpandWidth = false;
-      mappings = {
-        "<space>" = "none";
-        "n" = "move_cursor_down";
-        "e" = "move_cursor_up";
+      popupBorderStyle = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
+      closeIfLastWindow = true;
+      window = {
+        width = 40;
+        height = 15;
+        autoExpandWidth = false;
+        mappings = {
+          "<space>" = "none";
+          "n" = "move_cursor_down";
+          "e" = "move_cursor_up";
+        };
       };
     };
   };
+
   keymaps = [
     {
       action = "<cmd>Neotree focus<CR>";
