b9a59c123434331f229c4f4977e841e80db480d2
Mathias Aschwanden <mathias.aschwanden@gmail.com>
2026-02-08 15:11:35 +0100

    Formatting

--- flake.nix
+++ flake.nix
@@ -7,48 +7,44 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs =
-    {
-      nixpkgs,
-      flake-parts,
-      ...
-    }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = {
+    nixpkgs,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      perSystem =
-        {
-          pkgs,
-          system,
-          ...
-        }:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          lib = import ./lib {
-            inherit pkgs;
-            makeNixvimWithModule = inputs.nixvim.legacyPackages.${system}.makeNixvimWithModule;
-          };
-
-          nixvimMinimal = lib.mkNixvim {
-            copilot = false;
-          };
-          nixvimDefault = lib.mkNixvim {
-            copilot = true;
-          };
-        in
-        {
-          packages = {
-            default = nixvimDefault;
-            minimal = nixvimMinimal;
-          };
-
-          legacyPackages.lib = {
-            mkNixvim = lib.mkNixvim;
-          };
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = import ./lib {
+          inherit pkgs;
+          makeNixvimWithModule = inputs.nixvim.legacyPackages.${system}.makeNixvimWithModule;
         };
+
+        nixvimMinimal = lib.mkNixvim {
+          copilot = false;
+        };
+        nixvimDefault = lib.mkNixvim {
+          copilot = true;
+        };
+      in {
+        packages = {
+          default = nixvimDefault;
+          minimal = nixvimMinimal;
+        };
+
+        legacyPackages.lib = {
+          mkNixvim = lib.mkNixvim;
+        };
+      };
     };
 }
